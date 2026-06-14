fbadb4e0a835b114e9552368fe09688da352881b
Mairaj khalid <majubalti2008@gmail.com>
2026-06-14 10:29:21 +0500

    first program

---
+++ firstprogram.py
@@ -0,0 +1,46 @@
+#realational operator
+# a = 20
+# b =20
+# print(a==b)
+
+#arithmetic operator
+# num =10
+# num +=10
+# print("num", num)
+
+#logical operator
+# a =50
+# b =20
+# print(not False)
+# print(not (a<b))
+
+# type conversion
+# a = int("4")
+# b = 6
+# print(type(a))
+# print(a+b)
+
+# input
+# name = input("enter yout name: ")
+# print("wellcome", name)
+
+# name = input("Enter your name: ")
+# age = input("enter your age: ")
+# marks =input("enter your marks: ")
+# print("wellcome", name)
+# print("wellcome", age)
+# print("wellcome", marks)
+
+# practice1
+# num1 =int( input("enter first number"))
+# num2 =int(input("enter second number"))
+# print("sum= ",num1+num2)
+
+#practice2
+# side =float(input("enter the side"))
+# print("area", side*side)
+
+#practice3
+a= float(input("enter 1st number"))
+b= float(input("enter 2nd number"))
+print("average", (a+b)/2)
