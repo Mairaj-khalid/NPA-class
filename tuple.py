b3fce49eee7fc2d51d4e7e4713f13875e5e79f44
Mairaj khalid <majubalti2008@gmail.com>
2026-06-16 12:34:26 +0500

    tuple class

---
+++ tuple.py
@@ -0,0 +1,31 @@
+# tup = (1,2,3,4)
+# print(type(tup))
+
+# tup = (1,2,2,3,4)
+# print(tup.count(2))
+
+#practice1
+# movies = []
+# movie1 = str(input("enter your favourite movie1 : "))
+# movie2 = str(input("enter your favorite movie 2 : "))
+# movie3 = str(input("enter your movie3 : "))
+
+# movies.append(movie1)
+# movies.append(movie2)
+# movies.append(movie3)
+# print(movies)
+
+#practice2
+# list1 = [1,2,3,3,2,1]
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 ==copy_list1):
+#     print("pallindrome")
+# else:
+#     print("non pallindrome")
+
+#practice3
+grade = ("A","B","C","A","A","B","A")
+print(grade.count("A"))
+
